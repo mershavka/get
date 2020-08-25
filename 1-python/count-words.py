555fcd316ee892fc0bd4ad7505201de55e13d8c7
Иван Потылицын <ivan.phystech@gmail.com>
2020-08-25 19:42:56 +0300

    Добавил скрипт, читающий Толстого и Пушкина

---
+++ 1-python/count-words.py
@@ -0,0 +1,35 @@
+import pathlib
+import os
+import datetime
+
+
+clear = lambda: os.system('cls')
+clear()
+
+data_folder = pathlib.Path().absolute()
+print(data_folder)
+
+# name = input('Enter file:> ')
+
+name = "lev.txt"
+
+handle = open(os.path.join(data_folder, name), 'r') #функция для открытия файла с параметром 'r' - открытие на чтение (является значением по умолчанию)
+counts = dict() #создание словаря - неупорядоченной коллекции произвольных объектов с доступом по ключу
+
+start = datetime.datetime.now()
+ 
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1 #dict.get(key[, default]) - возвращает значение ключа, но если его нет, не бросает исключение, а возвращает default (по умолчанию None)
+ 
+bigcount = None
+bigword = None
+for word, count in list(counts.items()):
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+
+finish = datetime.datetime.now()
+
+print(bigword, bigcount, "За ", (start - finish).microseconds / 1e6, "с")
